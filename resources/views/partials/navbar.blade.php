08e24b4aa7404c251b0d5286dbc15874364143f8
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-07 16:26:49 +0700

    Update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
 	<div class="container">
-		<a class="navbar-brand" href="/">
+		<a class="navbar-brand" href="{{ route('index') }}">
 			Apri
 		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,38 +9,49 @@
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">
+					<a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="{{ route('index.home') }}">
 						Home
 					</a>
 				</li>   
 				<li class="nav-item">
-					<a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="/about">
+					<a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="{{ route('index.about') }}">
 						About
 					</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/gallery">
+					<a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="{{ route('index.home') }}">
 						Gallery
 					</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contact/create">
+					<a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="{{ route('contact.create') }}">
 						Contacts
 					</a>
 				</li>
 			</ul>
-			<ul class="navbar-nav ms-auto">
-				<li class="nav-item">
-					<a href="{{ route('login') }}" class="nav-link">
-						Login
-					</a>
-				</li>
-				<li class="nav-item">
-					<a href="{{ route('register') }}" class="nav-link">
-						Register
-					</a>
-				</li>
-			</ul>
+			
+			@if(Auth::guest())
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a href="{{ route('login') }}" class="nav-link">
+							Login
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="{{ route('register') }}" class="nav-link">
+							Register
+						</a>
+					</li>
+				</ul>
+			@else
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a href="{{ route('contact.index') }}" class="nav-link">
+							Dashboard
+						</a>
+					</li>
+				</ul>
+			@endif
 		</div>
 	</div>
 </nav>
