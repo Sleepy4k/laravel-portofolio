68cf2965154645458f93d631d0770b933bef276e
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 11:03:10 +0700

    Update dashboard sidebar

--- resources/views/partials/app/script.blade.php
+++ resources/views/partials/app/script.blade.php
@@ -1,11 +1,8 @@
-<!-- Jquery JS-->
 <script src="{{asset('admin/vendor/jquery-3.2.1.min.js')}}"></script>
 
-<!-- Bootstrap JS-->
 <script src="{{asset('admin/vendor/bootstrap-4.1/popper.min.js')}}"></script>
 <script src="{{asset('admin/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
 
-<!-- Vendor JS       -->
 <script src="{{asset('admin/vendor/slick/slick.min.js')}}"></script>
 <script src="{{asset('admin/vendor/wow/wow.min.js')}}"></script>
 <script src="{{asset('admin/vendor/animsition/animsition.min.js')}}"></script>
@@ -17,5 +14,4 @@
 <script src="{{asset('admin/vendor/chartjs/Chart.bundle.min.js')}}"></script>
 <script src="{{asset('admin/vendor/select2/select2.min.js')}}"></script>
 
-<!-- Main JS-->
 <script src="{{asset('admin/js/main.js')}}"></script>
--- resources/views/partials/app/sidebar.blade.php
+++ resources/views/partials/app/sidebar.blade.php
@@ -7,18 +7,36 @@
     <div class="menu-sidebar__content js-scrollbar1">
         <nav class="navbar-sidebar">
             <ul class="list-unstyled navbar__list">
-                <li class="has-sub">
-                    <a class="js-arrow" href="#">
-                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                    <ul class="list-unstyled navbar__sub-list js-sub-list">
-                        <li>
-                            <a href="{{ route('home') }}">Dashboard 1</a>
-                        </li>
-                    </ul>
+                <li>
+                    <a href="{{ route('home') }}">
+                    <i class="fas fa-chart-line"></i>
+                        Dashboard
+                    </a>
                 </li>
                 <li>
                     <a href="{{ route('contact.index') }}">
-                        <i class="fas fa-chart-bar"></i>Data Contact Us</a>
+                    <i class="fas fa-chart-bar"></i>
+                        Data Contact Us
+                    </a>
+                </li>
+                <li class="has-sub">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>
+                        Example
+                    </a>
+                    <ul class="list-unstyled navbar__sub-list js-sub-list">
+                        <li>
+                            <a href="{{ route('home') }}">
+                                Example 1
+                            </a>
+                            <a href="{{ route('home') }}">
+                                Example 2
+                            </a>
+                            <a href="{{ route('home') }}">
+                                Example 3
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>
