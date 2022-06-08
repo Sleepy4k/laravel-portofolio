38939551029196955c278b544fa6c18eeaa60175
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 17:06:13 +0700

    Add gallery migration

---
+++ database/migrations/2022_06_08_051126_create_galleries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("desc");
+            $table->string("link");
+            $table->string('image')->default('image_not_found');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
