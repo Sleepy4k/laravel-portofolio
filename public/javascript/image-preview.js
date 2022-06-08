8c625c60519a1988dd63cf4a009f3f99cfbbef5d
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 17:11:49 +0700

    Add image preview

---
+++ public/javascript/image-preview.js
@@ -0,0 +1,13 @@
+function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+        imgPreview.src = oFREvent.target.result;
+    }
+}
--- resources/views/partials/main/script.blade.php
+++ resources/views/partials/main/script.blade.php
@@ -1,1 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script type="text/javascript" href="{{ asset('javascript/jquery-3.3.1.min.js') }}"></script>
+<script type="text/javascript" href="{{ asset('javascript/owl.carousel.js') }}"></script>    
+<script type="text/javascript" href="{{ asset('javascript/bootstrap.min.js') }}"></script>
