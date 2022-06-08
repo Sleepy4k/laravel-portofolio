c8fcf268300910cb9ff21179b79820ae5e3222aa
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 11:02:50 +0700

    Add home button at header dropdown

--- resources/views/partials/app/header.blade.php
+++ resources/views/partials/app/header.blade.php
@@ -27,6 +27,11 @@
                                     </div>
                                 </div>
                                 <div class="account-dropdown__footer">
+                                    <a href="{{ route('index.home') }}">
+                                        Home
+                                    </a>
+                                </div>
+                                <div class="account-dropdown__footer">
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
