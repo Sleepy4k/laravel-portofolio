88d1aff971e0dd4ff555624438b6cefdc8311da2
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 11:01:04 +0700

    Slicing edit contact form

---
+++ resources/views/partials/app/form/edit_contact.blade.php
@@ -0,0 +1,52 @@
+<form action="{{route('contact.update', $contact->id)}}" method="post" class="">
+    @csrf
+    <div class="form-group">
+        <label class=" form-control-label">
+            Nama
+        </label>
+        <input type="text" name="nama" class="@error('nama') is-invalid @enderror form-control" value="{{$contact->nama}}">
+    </div>
+
+    @error('nama')
+        <span class="error-display">
+            {{ $message }}
+        </span>
+    @enderror
+
+    <div class="form-group">
+        <label class=" form-control-label">
+            Email
+        </label>
+        <input type="email" name="email" class="@error('email') is-invalid @enderror form-control" value="{{$contact->email}}">
+    </div>
+
+    @error('email')
+        <span class="error-display">
+            {{ $message }}
+        </span>
+    @enderror
+
+    <div class="form-group">
+        <label class=" form-control-label">
+            Pesan
+        </label>
+        <textarea class="@error('pesan') is-invalid @enderror form-control" style="height: 100px" name="pesan">{{$contact->pesan}}</textarea>
+    </div>
+
+    @error('pesan')
+        <span class="error-display">
+            {{ $message }}
+        </span>
+    @enderror
+
+    <div class="card-footer">
+        <button type="submit" class="btn btn-primary btn-sm">
+            <i class="fa fa-dot-circle-o"></i> 
+            Submit
+        </button>
+        <button type="reset" class="btn btn-danger btn-sm">
+            <i class="fa fa-ban"></i> 
+            Reset
+        </button>
+    </div>
+</form>
