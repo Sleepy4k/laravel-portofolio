503889492e045d26161077adc5cda675b55dfb68
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-08 11:01:20 +0700

    Slicing index contact table

--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -9,55 +9,20 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="overview-wrap">
-                            <h2 class="title-1">Data Contact Us</h2>
+                            <h2 class="title-1">
+                                Data Contact Us
+                            </h2>
                         </div>
                     </div>
                 </div>
             </div>
                 <div class="col-md-12">
                     <div class="table-responsive m-b-40">
-                        <table class="table table-borderless table-data3">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama</th>
-                                    <th>Email</th>
-                                    <th>Pesan</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($contacts as $index => $contact)
-                                    <tr>
-                                        <td>{{$index + 1}}</td>
-                                        <td>{{$contact->nama}}</td>
-                                        <td>{{$contact->email}}</td>
-                                        <td>{{$contact->pesan}}</td>
-                                        <td>
-                                            <a href="{{route('contact.edit', $contact->id)}}">
-                                                <i class="fas fa-edit"></i>
-                                            </a> 
-                                            | 
-                                            <a href="{{route('contact.destroy', $contact->id)}}" style="color:red">
-                                                <i class="fas fa-trash"></i>
-                                            </a>
-                                        </td>
-                                    </tr> 
-                                @endforeach
-                            </tbody>
-                        </table>
-                        {{$contacts->links()}}
+                        @include('partials.app.table.index_contact')
                     </div>
                 </div>
             </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="copyright">
-                            <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                @include('partials.app.footer')
         </div>
     </div>
 @endsection
---
+++ resources/views/partials/app/table/index_contact.blade.php
@@ -0,0 +1,32 @@
+<table class="table table-borderless table-data3">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Pesan</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($contacts as $index => $contact)
+            <tr>
+                <td>{{$index + 1}}</td>
+                <td>{{$contact->nama}}</td>
+                <td>{{$contact->email}}</td>
+                <td>{{$contact->pesan}}</td>
+                <td>
+                    <a href="{{route('contact.edit', $contact->id)}}">
+                        <i class="fas fa-edit"></i>
+                    </a> 
+                    | 
+                    <a href="{{route('contact.destroy', $contact->id)}}" style="color:red">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </td>
+            </tr> 
+        @endforeach
+    </tbody>
+</table>
+
+{{$contacts->links()}}
