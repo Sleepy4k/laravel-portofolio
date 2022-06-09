dd4180c6f0210ff9a5f64d98c82b6a8bf35fa959
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-09 20:44:51 +0700

    Add message when table is nill in contact

--- resources/views/partials/app/table/index_contact.blade.php
+++ resources/views/partials/app/table/index_contact.blade.php
@@ -1,31 +1,57 @@
 <table class="table table-borderless table-data3">
     <thead>
         <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Pesan</th>
-            <th>Aksi</th>
+            <th>
+                No
+            </th>
+            <th>
+                Nama
+            </th>
+            <th>
+                Email
+            </th>
+            <th>
+                Pesan
+            </th>
+            <th>
+                Aksi
+            </th>
         </tr>
     </thead>
     <tbody>
-        @foreach ($contacts as $index => $contact)
+        @if($contacts->count() <= 0)
             <tr>
-                <td>{{$index + 1}}</td>
-                <td>{{$contact->nama}}</td>
-                <td>{{$contact->email}}</td>
-                <td>{{$contact->pesan}}</td>
-                <td>
-                    <a href="{{route('contact.edit', $contact->id)}}">
-                        <i class="fas fa-edit"></i>
-                    </a> 
-                    | 
-                    <a href="{{route('contact.destroy', $contact->id)}}" style="color:red" class="button delete-confirm">
-                        <i class="fas fa-trash"></i>
-                    </a>
+                <td colspan=9 style="text-align: center;"> 
+                    Belum Ada Data Yang Tersedia 
                 </td>
-            </tr> 
-        @endforeach
+            </tr>
+        @else
+            @foreach ($contacts as $index => $contact)
+                <tr>
+                    <td>
+                        {{$index + 1}}
+                    </td>
+                    <td>
+                        {{$contact->nama}}
+                    </td>
+                    <td>
+                        {{$contact->email}}
+                    </td>
+                    <td>
+                        {{$contact->pesan}}
+                    </td>
+                    <td>
+                        <a href="{{route('contact.edit', $contact->id)}}">
+                            <i class="fas fa-edit"></i>
+                        </a> 
+                        | 
+                        <a href="{{route('contact.destroy', $contact->id)}}" style="color:red" class="button delete-confirm">
+                            <i class="fas fa-trash"></i>
+                        </a>
+                    </td>
+                </tr> 
+            @endforeach
+        @endif
     </tbody>
 </table>
 
