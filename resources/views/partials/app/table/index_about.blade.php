f4752484a229ac4da3cf65f4b0f6473c41a99027
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-09 19:53:39 +0700

    Add index table about in dashboard

---
+++ resources/views/partials/app/table/index_about.blade.php
@@ -0,0 +1,48 @@
+<table class="table table-borderless table-data3">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Judul</th>
+            <th>Name</th>
+            <th>Umur</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Bio</th>
+            <th>Foto</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($abouts as $index => $about)
+            <tr>
+                <td>{{$index + 1}}</td>
+                <td>{{$about->title}}</td>
+                <td>{{$about->name}}</td>
+                <td>{{$about->bday}}</td>
+                <td>{{$about->phone}}</td>
+                <td>{{$about->email}}</td>
+                <td>{{$about->bio}}</td>
+                @if(!empty(file_exists('storage/images/about/'.$about->image)))
+                    <td>
+                        <img height="35px" width="35px" src="{{ asset('storage/images/about/'.$about->image) }}"/>
+                    </td>
+                @else
+                    <td>
+                        <img height="35px" width="35px" src="{{ asset('admin/images/bg-title-01.jpg') }}"/>
+                    </td>
+                @endif
+                <td>
+                    <a href="{{route('about.edit', $about->id)}}">
+                        <i class="fas fa-edit"></i>
+                    </a> 
+                    | 
+                    <a href="{{route('about.destroy', $about->id)}}" style="color:red" class="button delete-confirm">
+                        <i class="fas fa-trash"></i>
+                    </a>
+                </td>
+            </tr> 
+        @endforeach
+    </tbody>
+</table>
+
+{{$abouts->links()}}
