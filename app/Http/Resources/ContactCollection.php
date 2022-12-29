a52e0d7ceea10fd5d8b04849e82b19ddab946796
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-29 21:13:14 +0700

    feat: contact collection
    add body transformer

---
+++ app/Http/Resources/ContactCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ContactCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return $this->collection;
+    }
+}
