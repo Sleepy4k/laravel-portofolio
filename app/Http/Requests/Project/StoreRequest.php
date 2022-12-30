7d486580d1c30ee53e4af603459b56e399ac9e3c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-30 17:22:45 +0700

    feat: project form request
    add request validation

---
+++ app/Http/Requests/Project/StoreRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'string', 'max:255', 'unique:projects,title'],
+            'description' => ['required', 'string'],
+            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,svg', 'max:4092', 'dimensions:min_width=100,min_height=100']
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'title' => trans('form.project.title'),
+            'description' => trans('form.project.description'),
+            'image' => trans('form.project.image')
+        ];
+    }
+}
---
+++ app/Http/Requests/Project/UpdateRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Project;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $id = basename(request()->path());
+
+        dd($this->title);
+        return [
+            'title' => ['required', 'string', 'max:255', 'unique:projects,title,' . $id],
+            'description' => ['required', 'string'],
+            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,svg', 'max:4092', 'dimensions:min_width=100,min_height=100']
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'title' => trans('form.project.title'),
+            'description' => trans('form.project.description'),
+            'image' => trans('form.project.image')
+        ];
+    }
+}
