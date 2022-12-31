7237e03fcbd43cb5d47b6abdb1ff6b2673b95f98
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-31 18:59:21 +0700

    chore: about repository
    add image handler

--- app/Repositories/Models/AboutRepository.php
+++ app/Repositories/Models/AboutRepository.php
@@ -3,12 +3,16 @@
 namespace App\Repositories\Models;
 
 use App\Models\About;
+use App\Traits\SystemLog;
+use App\Traits\UploadFile;
 use Illuminate\Database\Eloquent\Model;
 use App\Repositories\EloquentRepository;
 use App\Contracts\Models\AboutInterface;
 
 class AboutRepository extends EloquentRepository implements AboutInterface
 {
+    use UploadFile, SystemLog;
+
     /**
      * @var Model
      */
@@ -22,5 +26,27 @@ class AboutRepository extends EloquentRepository implements AboutInterface
     public function __construct(About $model)
     {
         $this->model = $model;
+    }
+
+    /**
+     * Update existing model.
+     *
+     * @param  int  $modelId
+     * @param  array  $payload
+     * @return Model
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        try {
+            $model = $this->findById($modelId);
+
+            if (array_key_exists('image', $payload)) {
+                $payload['image'] = $this->updateSingleFile('image', $payload['image'], $model->image);
+            }
+
+            return $model->update($payload);
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 }
